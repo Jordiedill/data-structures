e990141bedd340c317f4a259c1b677821f239844
Jordan Dillard <jordiedill@Jordans-MacBook-Pro.local>
2019-06-01 16:07:58 -0500

    Nearly completed linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,56 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if(list.head === null){
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function() {
-  };
+    var removedVal = list.head.value;
+
+    if(list.head === null) {
+      return undefined;
+    }
+
+
+
+    if (list.head === list.tail) {
+      list.head = null;
+      list.tail= null;
+      return removedVal;
+    }
+
+
+    list.head = list.head.next
+
+    return removedVal;
+
+  }
 
   list.contains = function(target) {
+
+    if(list.head != null){
+
+        var recurse = function (obj) {
+          if(obj.value === target){
+            return true;
+          }
+          if(obj.next === null){
+            return false;
+          }
+          return recurse(obj.next);
+        }
+
+      recurse(list.head.next);
+
+    }
+
   };
 
   return list;
